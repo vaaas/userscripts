2478f19382e13b412f015b43f1149facc916e62e
Vasileios Pasialiokis <whiterocket@outlook.com>
2019-08-05 04:23:32 +0300

    refactor

--- the_script_makes_it_sound_cool.js
+++ the_script_makes_it_sound_cool.js
@@ -1,67 +1,86 @@
- (function() {
+(function() {
 "use strict"
 
-const E = s => document.createElement(s)
+const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x)
+
+const parse_html = x => new DOMParser.parseFromString(x, "text/html")
+
+function E(tag)
+	{ this.element = document.createElement(tag) }
+
+E.prototype.attribute = function(k) { return function(x)
+	{ this.element.setAttribute(k, x)
+	return this }}
+
+E.prototype.value = function(k) { return function (x)
+	{ this.element[k] = x
+	return this }}
+
+E.prototype.style = E.attribute("style")
+
+E.prototype.href = E.attribute("href")
+
+E.prototype.src = E.attribute("src")
+
+E.prototype.onclick = E.value("onclick")
+
+E.prototype.innerHTML = E.value("innerHTML")
+
+E.prototype.child = function(x)
+	{ this.element.appendChild(x)
+	return this }
 
 function empty(el) { while(el.firstChild) el.removeChild(el.firstChild) }
 
-function replace(el, tree) {
-	empty(el)
+const replace = el => tree =>
+	{ empty(el)
 	el.appendChild(tree) }
 
 const is_gallery_page = location => new RegExp("^/s/[^/]+/[^/]+$").test(location)
 
-const parse_page = dom => ({
-	"title": dom.querySelector("h1").innerHTML,
+const parse_page = dom =>
+	({ "title": dom.querySelector("h1").innerHTML,
 	"back": dom.querySelector("div.sb a").href,
 	"next": dom.querySelector("#next").href,
 	"prev": dom.querySelector("#prev").href,
 	"pic": dom.querySelector("#img").src })
 
-function render_page(data) {
-	const body = E("body")
-	body.setAttribute("style", "margin: 0; background: black; text-align: center;")
-	const h1 = E("h1")
-	const a = E("a")
-	a.href = data.back
-	a.innerHTML = data.title
-	const img = E("img")
-	img.setAttribute("style", "display: block; max-width: 100vw; margin: auto;")
-	img.src = data.pic
-	img.onclick = image_clicked(data.prev, data.next)
-	body.appendChild(img)
-	body.appendChild(h1)
-	h1.appendChild(a)
-	return body }
+function render_page(data)
+	{ return E("body")
+	.style("margin: 0; background: black; text-align: center;")
+	.child(E("h1")
+		.child(E("a").href(data.back).innerHTML(data.title)))
+	.child("img")
+		.style("display: block; width: 100vw; margin: auto;")
+		.src(data.pic)
+		.onclick(image_clicked(data.prev, data.next))
+	.element }
 
 const go_fullscreen = el => el.requestFullscreen()
 
-const image_clicked = (prev, next) => event => {
-	if (event.clientY < window.innerHeight / 5 && !document.fullscreenElement)
+const image_clicked = (prev, next) => event =>
+	{ if (event.clientY < window.innerHeight / 5 && !document.fullscreenElement)
 		go_fullscreen(document.firstChild)
 	else if ((event.clientX - event.target.offsetLeft) > event.target.offsetWidth/2)
 		get_page(next)
 	else
 		get_page(prev) }
 
-const get = url => new Promise(res => {
-	const req = new XMLHttpRequest()
+const get = url => new Promise(yes =>
+	{ const req = new XMLHttpRequest()
 	req.open("GET", url)
-	req.onload = _ => res(req.responseText)
-	req.send() })
+	req.onload = () => yes(req.responseText)
+	req.send()})
 
-async function get_page(url) {
-	replace(document.firstChild,
-	render_page(
-	parse_page(
-	new DOMParser().parseFromString(
-	await get(url),"text/html"))))}
+const get_page = x => get(x).then(pipe
+	(parse_html,
+	parse_page,
+	render_page,
+	replace(document.firstChild)))
 
 function main() {
 	if (!is_gallery_page(window.location.pathname)) return
-	const new_root = E("html")
-	new_root.appendChild(render_page(parse_page(document)))
-	replace(document, new_root)}
+	replace(document)(E("html").child(render_page(parse_page(document))).element) }
 
 if (window.readyState === "complete") main()
 else window.onload = main
