a11fe80b941369429bbb7fe398535e7e72bee991
Vasileios Pasialiokis <whiterocket@outlook.com>
2020-11-15 14:56:00 +0200

    navigation!

--- ao3_spa.js
+++ ao3_spa.js
@@ -13,15 +13,37 @@
 (function() {
 'use strict'
 
+const G = {}
+const Anonymous = Symbol()
+
 const style = `
-html { background: #eeeeee; }
+html
+	{ background: #eee;
+	font-size: 18px; }
 
 a
-	{ color: #b71c1c;
+	{ color: #b22;
 	text-underline-offset: 0.25em;
 	text-decoration-style: dotted; }
 
 a:hover { text-decoration-style: solid; }
+
+.work
+	{ max-width: 40rem;
+	padding: 0.5rem;
+	margin: 0.5rem auto;
+	background: #fff;
+	border: 1px solid #ddd;
+	border-radius: 4px; }
+
+.work > h1 { text-align: center; }
+
+.work > .summary
+	{ max-width: 20rem;
+	margin: auto;
+	color: #444;
+	padding-bottom: 1rem;
+	border-bottom: 1px solid #aaa; }
 
 .results
 	{ display: flex;
@@ -29,9 +51,9 @@ a:hover { text-decoration-style: solid; }
 	justify-content: center; }
 
 .works article
-	{ background: #fafafa;
-	border: 1px solid #e0e0e0;
-	border-bottom: 1px solid #bdbdbd;
+	{ background: #fff;
+	border: 1px solid #ddd;
+	border-bottom: 1px solid #ccc;
 	border-radius: 4px;
 	padding: 0.5rem;
 	margin: 0.5rem;
@@ -41,11 +63,11 @@ a:hover { text-decoration-style: solid; }
 
 .works article:hover {
 	border-color: transparent;
-	box-shadow: 0 0 0 2px #ff1744 }
+	box-shadow: 0 0 0 2px #f24; }
 
 .tags a
 	{ display: inline-block;
-	margin-right: 0.25em; }
+	margin-right: 0.5em; }
 
 h1
 	{ margin: 0;
@@ -58,69 +80,43 @@ h1
 `
 
 function main()
-	{ empty(document.firstElementChild)
+	{ G.route = new Observable(null)
+	empty(document)
+	document.appendChild(
+		E.of('html')
+		.child(E.of('head')
+			.child(E.of('title').text('AO3 SPA'))
+			.child(E.of('style').text(style)))
+		.child(E.of('body')
+			.child(E.of('div').add_class('router')
+				.on(G.route, me => x => me.clear().child(x))))
+		.element)
+	G.route.map(N(Works)) }
 
-	P(document.firstElementChild,
-	N(E),
-	E.child(P(new E('head'),
-		E.child(P(new E('title'),
-			E.text('AO3 SPA'))),
-		E.child(P(new E('style'),
-			 E.text(style))))),
-	E.child(P(new E('body'),
-		E.child(new Works())))) }
-
-// COMBINATORS
-const just = a => () => a
-const C = a => b => c => a(c)(b)
-
-// FUNCTIONS
-function P (x, ...fs)
-	{ for (const f of fs) x = f(x)
-	return x }
-
-function PP (...fs)
-	{ return function (x)
-		{ for (const f of fs) x = f(x)
-		return x }}
-
-function empty (x)
-	{ while (x.firstChild) x.firstChild.remove()
-	return x }
-
-function parse_work(x)
-	{ const o = {}
-	let e
-	e = qs('h4.heading a')(x)
-	o.href = e.href
-	o.title = e.innerText
-	e = qs('h4.heading a[rel="author"]')(x)
-	o.author = e.innerText
-	e = qs('.summary')(x)
-	o.summary = e ? e.innerHTML : ''
-	e = qss('ul.tags a.tag')(x)
-	o.tags = e.map(innertext)
-	e = qs('.datetime')(x)
-	o.date = e.innerText
-	return o }
-
+const tap = f => x => { f(x) ; return x }
+const ftap = f => x => { f(x) ; return false }
+const K = a => () => a
+const A = (x, ...fs) => fs.reduce((a,b)=>b(a), x)
+const AA = (...fs) => x => fs.reduce((a,b)=>b(a), x)
+const T = a => b => b(a)
 const map = f => x => x.map(f)
-const mapped = x => f => x.map(f)
-const filter = f => x => x.map(f)
 const target = x => x.target
 const value = x => x.value
-const keycode = x => x.keyCode
-const log = x => console.log(x)
 const N = c => x => new c(x)
 const join = s => xs => xs.join(s)
-const serialise_params = PP(Object.entries, map(map(encodeURIComponent)), map(join('=')), join('&'))
+const serialise_params = AA(Object.entries, map(map(encodeURIComponent)), map(join('=')), join('&'))
 const then = f => p => p.then(f)
-const prefix = a => b => a+b
-const suffix = a => b => b+a
+const add = a => b => a+b
 const parseHTML = x => new DOMParser().parseFromString(x, 'text/html')
 const qs = x => e => e.querySelector(x)
 const qss = x => e => Array.from(e.querySelectorAll(x))
-const innertext = x => x.innerText
+const inner_text = x => x.innerText
+const href = x => x.href
+const remove = x => x.remove()
+const empty = tap(x => { while(x.firstChild) x.firstChild.remove() })
+const maybe = f => x => x === null ? x : f(x)
+const nothing = f => x => x !== null ? x : f(x)
+const pluck = k => x => x[k]
 
 const get = x => new Promise(yes =>
 	{ const req = new XMLHttpRequest()
@@ -128,143 +124,219 @@ const get = x => new Promise(yes =>
 	req.open('GET', x)
 	req.send() })
 
-const search = a => PP(serialise_params, prefix('?'), prefix(a), get)
-const search_works = PP(x => ({'utf8': '✓', 'work_search[query]': x}), search('/works/search'))
+const search = x => AA(serialise_params, add('?'), add(x), get)
+const search_works = AA(x => ({'utf8': '✓', 'work_search[query]': x}), search('/works/search'))
 
-// CLASSES
-function Functor (x) { this.x = x }
-	Functor.map = f => x =>
-		{ x.x = f(x.x)
-		return x }
+class Observable
+	{ constructor(x)
+		{ this.x = x
+		this.watchers = new Set() }
+	static of (x) { return new Observable(x) }
+	map(f)
+		{ this.x = f(this.x)
+		this.notify()
+		return this }
+	get() { return this.x }
+	each(f) {
+		this.watchers.forEach(f)
+		return this }
+	notify() { return this.each(T(this.x)) }
+	watch(f)
+		{ this.watchers.add(f)
+		return this }
+	unwatch(f)
+		{ this.watchers.delete(f)
+		return this }}
 
-	Functor.get = x => x.x
+class E
+	{ constructor(x)
+		{ this.element = document.createElement(x)
+		this.watches = new Map()
+		this._children = new Set()
+		this.watchers = new Map() }
+	static of (x) { return new E(x) }
+	child(x)
+		{ x.parent = this
+		this._children.add(x)
+		this.element.appendChild(x.element)
+		return this }
+	children(xs)
+		{ this.clear()
+		xs.forEach(x => this.child(x))
+		return this }
+	clear() { return this.each(remove) }
+	remove()
+		{ this.clear()
+		if (this.parent)
+			this.parent._children.delete(this)
+		this.element.remove()
+		this.watches.forEach((f, o) => o.unwatch(f))
+		return this }
+	on(o, f)
+		{ const g = f(this)
+		o.watch(g)
+		if (this.watches.has(o))
+			this.watches.get(o).push(g)
+		else
+			this.watches.set(o, [g])
+		return this }
+	value(x)
+		{ this.element.value = x
+		return this }
+	focus(f)
+		{ this.element.onfocus = f
+		return this }
+	unfocus(f)
+		{ this.element.addEventListener('focusout', f)
+		return this }
+	style(x)
+		{ this.element.style = x
+		return this }
+	input(f)
+		{ this.element.oninput = f
+		return this }
+	keydown(f)
+		{ this.element.onkeydown = f
+		return this }
+	enter(f)
+		{ return this.keydown(x => x.keyCode === 13 ? f(x) : true) }
+	href(x)
+		{ this.element.href = x
+		return this }
+	text(x)
+		{ this.element.innerText = x
+		return this }
+	html(x)
+		{ this.element.innerHTML = x
+		return this }
+	click(f)
+		{ this.element.onclick = f
+		return this }
+	each(f)
+		{ for (const x of this._children.values())
+			f(x)
+		return this }
+	add_class(x)
+		{ this.element.classList.add(x)
+		return this }
+	bind(o)
+		{ this.element.value = o.get()
+		this.element.onchange = () => o.map(K(this.element.value))
+		this.on(o, me => x =>
+			{ if (me.element.value === x) return
+			me.element.value = x })
+		return this }
+	emit(event)
+		{ return x =>
+			{ if (this.watchers.has(event))
+				this.watchers.get(event).forEach(T(x))
+			return this }}
+	listen(event, f)
+		{ if (!this.watchers.has(event))
+			this.watchers.set(event, [])
+		this.watchers.get(event).push(f)
+		return this }}
 
-function Observable (x)
-	{ Functor.call(this, x)
-	this.watchers = new Set() }
+class Tag extends E
+	{ constructor(x)
+		{ super('a')
+		this.text(x).href(`/tags/${encodeURIComponent(x)}/works`) }}
 
-	Observable.map = f => x =>
-		{ x.x = f(x.x)
-		return Observable.notify(x) }
+class SearchResult extends E
+	{ constructor(x)
+		{ super('article')
+		.child(E.of('h1')
+			.child(E.of('a')
+				.text(x.title)
+				.href(x.href)
+				.click(SearchResult.title_clicked)))
+		.child(E.of('p').html(x.summary))
+		.child(E.of('div')
+			.add_class('tags')
+			.children(
+				x.tags.map(x =>
+					new Tag(x)
+					.click(ftap(this.emit('tag')))))) }
 
-	Observable.mapped = x => f =>
-		{ x.x = f(x.x)
-		return Observable.notify(x) }
+	static title_clicked(x)
+		{ get(x.target.href).then(AA(
+			parseHTML,
+			N(Work),
+			K,
+			map,
+			T(G.route)))
+		return false }}
 
-	Observable.notify = x =>
-		{ for (let f of x.watchers) f(x.x)
-		return x }
+class Work extends E
+	{ constructor(x)
+		{ super('div').add_class('work')
+		const work = Work.parse_work(x)
+		this.child(E.of('h1').text(work.title))
+		.child(E.of('aside').add_class('summary').html(work.summary))
+		.child(E.of('main').html(work.content)) }
 
-	Observable.watch = f => x =>
-		{ x.watchers.add(f)
-		return x }
+	static parse_work(x)
+		{ const work = {}
+		work.title = qs('#workskin h2.title')(x).innerText.trim()
+		work.summary = qs('#workskin div.summary blockquote.userstuff')(x).innerHTML.trim()
+		work.author = qs('#workskin div.preface h3.byline a[rel="author"]')(x).innerText.trim()
+		work.content = qs('#workskin #chapters div.userstuff')(x).innerHTML.trim()
+		return work }}
 
-	Observable.unwatch = f => x =>
-		{ x.watchers.delete(f)
-		return x }
+class Works extends E
+	{ constructor(x)
+		{ super('div')
+		const results = new Observable(null)
+		const text = new Observable(null)
 
-function E (x)
-	{ this.element = typeof x == 'string' ? document.createElement(x) : x
-	this._children = new Set()
-	this.parent = null
-	this.observes = new Map() }
+		const onenter = AA(
+			target,
+			value,
+			search_works,
+			then(AA
+				(parseHTML,
+				qss('li.work'),
+				map(Works.parse_work),
+				K,
+				map,
+				T(results))))
 
-	E.child = x => e =>
-		{ e._children.add(x)
-		x.parent = e
-		e.element.appendChild(x.element)
-		return e }
+		const tag_clicked = ftap(AA(
+			target,
+			tap(AA(inner_text, K, map, T(text))),
+			href, get, then(AA
+				(parseHTML,
+				qss('li.work'),
+				map(Works.parse_work),
+				K,
+				map,
+				T(results)))))
 
-	E.children = xs => e =>
-		{ e._children.forEach(E.remove)
-		xs.forEach(C(E.child)(e))
-		return e }
+		this.add_class('works')
+		.child(E.of('input').enter(onenter).bind(text))
+		.child(E.of('main').add_class('results')
+			.on(results, me => xs => me.children(
+				xs.map(x =>
+					new SearchResult(x)
+					.listen('tag', tag_clicked)))))
 
-	E.remove = e => 
-		{ if (!e.parent) return e
-		e.parent._children.delete(e)
-		e.element.remove()
-		e.observes.forEach((v, k) => unwatch(k)(v)) }
+		results.map(K(x instanceof Array ? x : [])) }
 
-	E.text = x => e =>
-		{ e.element.appendChild(document.createTextNode(x))
-		return e }
-
-	E.html = x => e =>
-		{ e.element.innerHTML = x
-		return e }
-
-	E.href = x => e =>
-		{ e.element.href = x
-		return E.click(just(false))(e) }
-
-	E.type = x => e =>
-		{ e.element.type = x
-		return e }
-
-	E.add_class = x => e =>
-		{ e.element.classList.add(x)
-		return e }
-
-	E.remove_class = x => e =>
-		{ e.element.classList.remove(x)
-		return e }
-
-	E.click = f => e =>
-		{ e.element.onclick = f
-		return e }
-
-	E.enter = f => e =>
-		{ e.element.onkeydown = x => { if (e.keyCode == 13) f(e) }
-		return e }
-
-	E.on = f => o => e =>
-		{ f = C(f)(e)
-		if (e.observes.has(o))
-			e.observes.get(o).push(f)
-		else e.observes.set(o, [f])
-		Observable.watch(f)(o)
-		return e }
-
-// COMPONENTS
-function Works()
-	{ E.call(this, 'div')
-
-	const results = new Observable([])
-
-	const tag = x => P(new E('a'), E.text(x), E.href(`/tags/${encodeURIComponent(x)}/works`))
-
-	const searchresult = x => P(new E('article'),
-		E.child(P(new E('h1'),
-			E.child(P(new E('a'),
-				E.text(x.title),
-				E.href(x.href))))),
-		E.child(P(new E('p'),
-			E.html(x.summary))),
-		E.child(P(new E('div'),
-			E.add_class('tags'),
-			E.children(x.tags.map(tag)))))
-
-	const onenter = PP(target,
-		value,
-		search_works,
-		then(PP
-			(parseHTML,
-			qss('li.work'),
-			map(parse_work),
-			just,
-			Observable.mapped(results))))
-
-	P(this,
-	E.add_class('works'),
-	E.child(P(new E('input'),
-		E.type('text'),
-		E.enter(onenter))),
-	E.child(P(new E('main'),
-		E.add_class('results'),
-		E.on
-			(PP(map(N(searchresult)), E.children))
-			(results)))) }
+	static parse_work(x)
+		{ const o = {}
+		let e
+		e = qs('h4.heading a')(x)
+		o.href = e.href
+		o.title = e.innerText
+		e = qs('h4.heading a[rel="author"]')(x)
+		o.author = e ? e.innerText : Anonymous
+		e = qs('.summary')(x)
+		o.summary = e ? e.innerHTML : ''
+		e = qss('ul.tags a.tag')(x)
+		o.tags = e.map(inner_text)
+		e = qs('.datetime')(x)
+		o.date = e.innerText
+		return o }}
 
 main()
 })()
