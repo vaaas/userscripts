40ca8ee7ed3a26aec59d71497bdb8834cf5a6786
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-02-25 17:20:37 +0200

    styles

--- ao3_spa.js
+++ ao3_spa.js
@@ -46,9 +46,13 @@ const falsify = always(false)
 const set = k => v => tap(o => o[k] = v)
 const pluck = k => x => x[k]
 
-// fetch
+// promises
+const then = f => x => x.then(f)
+
+// fetch, url
 const text = x => x.text()
-const search = (x, q) => P(new URL(loc(x)), set('search')(new URLSearchParams(q).toString()), fetch)
+const search_url = (x, q) => P(new URL(loc(x)), set('search')(new URLSearchParams(q).toString()))
+const search = (x, q) => fetch(search_url(x,q))
 
 // dom
 const parse_html = x => new DOMParser().parseFromString(x, 'text/html')
@@ -63,14 +67,15 @@ const log = tap(console.log)
 const G = {}
 
 function main()
-	{ G.root = document.body
-	empty(G.root)
+	{ G.route = Observable.of(null)
 	P(qss('style')(document), each(x => x.remove()))
 	P(qss('link')(document), each(x => x.remove()))
 	document.title = 'AO3 SPA'
-	G.route = Observable.of(null)
-	compute_element(route => ({ children: route === null ? [] : [route] }), G.route)(G.root)
-	document.body.parentElement.appendChild(P(elem('style'), set('innerText')(STYLESHEET)))
+	const body = document.body
+	empty(body)
+	body.parentElement.appendChild(P(elem('style'), set('innerText')(STYLESHEET)))
+	body.appendChild(Nav())
+	body.appendChild(Root(G.route))
 	G.route.map(just(WorkSearch())) }
 
 function Observable(x)
@@ -117,79 +122,185 @@ const compute_element = (f, ...xs) => tap(e => multi_watch((...xs) =>
 
 // business logic
 const STYLESHEET = `
+a { color: #933; }
+p { margin: 0; margin-bottom: 1em; }
+html, body, #root { height: 100vh; }
+html
+	{ line-height: 1.5em;
+	background: #eee;
+	font-size: 18px; }
+body { margin: 0; }
+#root
+	{ margin-left: 3.5rem;
+	overflow: auto; }
+nav
+	{ position: absolute;
+	background: #933;
+	width: 3rem;
+	font-size: 1.5em;
+	padding-top: 0.25rem;
+	text-align: center;
+	top: 0; left: 0; bottom: 0;
+	box-shadow: 5px 0px 10px rgba(0,0,0,0.3); }
+nav > div
+	{ width: 3rem;
+	line-height: 3rem; }
+nav > div:hover
+	{ background: rgba(255,255,255,0.5);
+	cursor: pointer; }
+input,
+#results article,
+section.work
+	{ box-shadow: 0px 3px 5px rgba(0,0,0,0.2);
+	border-radius: 0.25rem;
+	background-color: #f8f8f8;
+	transition: box-shadow ease 0.5s, background-color ease 0.5s; }
+input:hover,
+input:focus,
+#results article:hover
+	{ box-shadow: 0px 7px 20px rgba(0,0,0,0.4);
+	background-color: #fff; }
 input
+	{ outline: none;
+	border: none;
+	font-size: 1rem;
+	padding: 0.5rem; }
+section.search input
 	{ width: 20em;
 	display: block;
-	margin: auto; }
+	margin: auto;
+	margin-top: 1rem; }
+input:focus { outline: none; }
 #results
 	{ display: flex;
 	flex-wrap: wrap;
 	justify-content: center }
 #results article
-	{ width: 30em;
+	{ width: 20em;
 	height: 30ex;
 	overflow: auto;
 	margin: 1em;
-	padding: 1em;
-	border: 1px solid gray; }
+	padding: 1em; }
+#results article h1 { margin: 0; }
+section.work
+	{ max-width: 40em;
+	background-color: #fff;
+	padding: 2rem;
+	margin: auto; }
 `
 
 const Anonymous = Symbol()
 
 const loc = x => 'https://archiveofourown.org' + x
 
-const parse_work = x =>
-	({ href: P(x,
-		qs('h4.heading a'),
-		maybe(pluck('href')),
-		nothing(just(''))),
-	title: P(x,
-		qs('h4.heading a'),
-		maybe(pluck('innerText')),
-		nothing(just(''))),
-	author: P(x,
-		qs('h4.heading a[rel="author"]'),
-		maybe(pluck('innerText')),
-		nothing(just(Anonymous))),
-	summary: P(x,
-		qs('.summary'),
-		maybe(pluck('innerHTML')),
-		nothing(just(''))),
-	tags: P(x,
-		qss('ul.tags a.tag'),
-		map(pluck('innerHTML')),
-		Array.from),
-	date: P(x,
-		qs('.datetime'),
-		maybe(pluck('innerText')),
-		nothing(just(''))), })
+function Root(route)
+	{ return P(elem('section'),
+		set('id')('root'),
+		compute_element(route => ({ children: route === null ? [] : [route] }), route)) }
 
-const parse_results = PP(qss('li.work'), map(parse_work), Array.from)
-
-const render_results = x => P(elem('article'),
-	child(P(elem('h1'), set('innerText')(x.title))),
-	child(P(elem('p'), set('innerHTML')(x.summary))))
+function Nav()
+	{ return P(elem('nav'),
+		child(P(elem('div'),
+			set('innerText')('🔍'),
+			set('onclick')(() => G.route.map(just(WorkSearch())))))) }
 
 function WorkSearch()
 	{ const results = new Observable(null)
+	const url = new Observable(null)
 
-	const get_results = x =>
-		search('/works/search', {'utf8': '✓', 'work_search[query]': x.target.value })
-		.then(text)
-		.then(PP(
+	const parse_work = x =>
+		({ href: P(x,
+			qs('h4.heading a'),
+			maybe(pluck('href')),
+			nothing(just(''))),
+		title: P(x,
+			qs('h4.heading a'),
+			maybe(pluck('innerText')),
+			nothing(just(''))),
+		author: P(x,
+			qs('h4.heading a[rel="author"]'),
+			maybe(pluck('innerText')),
+			nothing(just(Anonymous))),
+		summary: P(x,
+			qs('.summary'),
+			maybe(pluck('innerHTML')),
+			nothing(just(''))),
+		tags: P(x,
+			qss('ul.tags a.tag'),
+			map(pluck('innerHTML')),
+			Array.from),
+		date: P(x,
+			qs('.datetime'),
+			maybe(pluck('innerText')),
+			nothing(just(''))), })
+
+	const parse_results = PP(qss('li.work'), map(parse_work), Array.from)
+
+	const get_results = x => url.map(just(search_url('/works/search', {'utf8': '✓', 'work_search[query]': x.target.value })))
+
+	const render_results = x => P(elem('article'),
+		child(P(elem('h1'),
+			child(P(elem('a'),
+				set('href')(x.href),
+				set('onclick')(falsify(() => G.route.map(just(WorkDisplay(x.href))))),
+				set('innerText')(x.title))))),
+		child(P(elem('p'), set('innerHTML')(x.summary))))
+
+	url.watch(PP(fetch,
+		then(text),
+		then(PP(
 			parse_html,
 			parse_results,
 			map(render_results),
 			Array.from,
 			just,
-			results.map.bind(results)))
+			results.map.bind(results)))))
 
-	return P(elem('div'),
+	return P(elem('section'),
+		set('className')('search'),
 		child(P(elem('input'),
 			set('onchange')(get_results))),
 		child(P(elem('div'),
 			set('id')('results'),
 			compute_element(x => ({ children: x }), results)))) }
+
+function WorkDisplay(x=null)
+	{ const url = new Observable(null)
+	const results = new Observable(null)
+
+	const parse_work = x => ({
+		body: P(x,
+			qs('#workskin #chapters .userstuff'),
+			maybe(pluck('innerHTML')),
+			nothing(just('<em>Work intentionally left blank.</em>'))),
+		summary: P(x,
+			qs('#workskin .summary blockquote.userstuff'),
+			maybe(pluck('innerHTML')),
+			nothing(just('<em>Summary intentionally left blank.</em>'))),
+	})
+
+	const render_work = x => [
+		P(elem('header'),
+			child(P(elem('blockquote'),
+				set('className')('summary'),
+				set('innerHTML')(x.summary)))),
+		P(elem('main'), set('innerHTML')(x.body)),
+	]
+
+	url.watch(PP(fetch,
+		then(text),
+		then(PP(
+			parse_html,
+			parse_work,
+			render_work,
+			just,
+			results.map.bind(results)))))
+
+	url.map(just(x))
+
+	return P(elem('section'),
+		set('className')('work'),
+		compute_element(x => ({ children: x }), results)) }
 
 main()
 })()
